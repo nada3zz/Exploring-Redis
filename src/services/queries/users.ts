133faa777a14a7fcaeaee4cf425cacaab525de2f
nadaeezzdin <nadaeezzdin@gmail.com>
2024-01-10 19:51:15 +0200

    add usernames to sorted set

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,11 +1,20 @@
-import { usernamesUniqueKey, usersKey } from '$services/keys';
+import { usernamesUniqueKey, usersKey,  usernamesKey } from '$services/keys';
 import { client } from '$services/redis';
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 
 
 
-export const getUserByUsername = async (username: string) => {};
+export const getUserByUsername = async (username: string) => {
+    const decimalId = await client.zScore(usernamesKey(), username);
+    if( !decimalId ){
+        throw new Error('User does not exist');
+    }
+
+    const id = decimalId.toString(16);
+    const user = await client.hGetAll(usersKey(id));
+    return deserialize(id, user);
+};
 
 export const getUserById = async (id: string) => {
     const user = await client.hGetAll(usersKey(id));
@@ -21,6 +30,10 @@ export const createUser = async (attrs: CreateUserAttrs) => {
     }
     await client.hSet(usersKey(id), serialize(attrs));
     await client.sAdd(usernamesUniqueKey(), attrs.username);
+    await client.zAdd(usernamesKey(), {
+        value: attrs.username,
+        score: parseInt(id, 16)
+    });
 
     return id;
 };
