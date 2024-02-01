06b93f1331ed7bcac153822090e313092723f3aa
nadaeezzdin <nadaeezzdin@gmail.com>
2024-02-01 23:55:21 +0200

    solving multiple bids with transaction

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,26 +1,45 @@
 import type { CreateBidAttrs, Bid } from '$services/types';
-import { bidHistoryKey } from '$services/keys';
+import { bidHistoryKey, itemsKey } from '$services/keys';
 import { client } from '$services/redis';
 import { DateTime } from 'luxon';
-import { serialize } from 'cookie';
-
+import { getItem } from './items';
 export const createBid = async (attrs: CreateBidAttrs) => {
-	const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
+	return client.executeIsolated(async (isolatedClient) => {
+		await isolatedClient.watch(itemsKey(attrs.itemId));
 
-	return client.rPush(bidHistoryKey(attrs.itemId), serialized);
+		const item = await getItem(attrs.itemId);
+		if (!item) {
+			throw new Error('Item does not exist');
+		}
+		if (item.price >= attrs.amount) {
+			throw new Error('Bid too low');
+		}
+		if (item.endingAt.diff(DateTime.now()).toMillis() < 0) {
+			throw new Error('Item closed to bidding');
+		}
+
+		const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
+
+		return isolatedClient
+		.multi()
+		.rPush(bidHistoryKey(attrs.itemId), serialized)
+		.hSet(itemsKey(item.id), {
+				bids: item.bids + 1,
+				price: attrs.amount,
+				highestBidUserId: attrs.userId
+		})
+		.exec();
+		
+	});
 };
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
 	const startIndex = -1 * offset - count;
 	const endIndex = -1 - offset;
 
-	const range = await client.lRange(
-		bidHistoryKey(itemId),
-		startIndex,
-		endIndex
-	);
+	const range = await client.lRange(bidHistoryKey(itemId), startIndex, endIndex);
 
-	return range.map(bid => deserializeHistory(bid));
+	return range.map((bid) => deserializeHistory(bid));
 };
 
 const serializeHistory = (amount: number, createdAt: number) => {
@@ -34,4 +53,4 @@ const deserializeHistory = (stored: string) => {
 		amount: parseFloat(amount),
 		createdAt: DateTime.fromMillis(parseInt(createdAt))
 	};
-}
+};
